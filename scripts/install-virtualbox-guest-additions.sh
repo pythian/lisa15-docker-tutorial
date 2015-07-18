10b94481250f4ab7d79f493f0442052d20bf74a5
Dimitrios Liappis <liappis@pythian.com>
2015-07-19 01:11:12 +0300

    Committing script used by vbox packer to install guest additions

---
+++ scripts/install-virtualbox-guest-additions.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Install needed packages
+PACKAGES="
+dkms
+build-essential
+"
+apt-get -y install $PACKAGES
+
+# Mount the disk image
+cd /tmp
+mkdir /tmp/isomount
+mount -t iso9660 -o loop /home/vagrant/VBoxGuestAdditions.iso /tmp/isomount
+
+# Install the drivers
+/tmp/isomount/VBoxLinuxAdditions.run
+
+# Cleanup
+umount isomount
+rm -rf isomount /root/VBoxGuestAdditions.iso
